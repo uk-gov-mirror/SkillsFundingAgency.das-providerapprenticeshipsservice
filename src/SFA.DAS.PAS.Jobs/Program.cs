c4f5d852ddc2cb1d10fdf7ff4dc9c2fc0e3f271a
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-17 16:54:03 +0100

    CSP-1955 Refactored Program.cs into static Program class with Main

--- src/SFA.DAS.PAS.Jobs/Program.cs
+++ src/SFA.DAS.PAS.Jobs/Program.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Configuration;
@@ -8,29 +10,38 @@ using Microsoft.Extensions.Logging;
 using SFA.DAS.PAS.Jobs.Extensions;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Extensions;
 
-FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+namespace SFA.DAS.PAS.Jobs;
 
-builder.AddConfiguration();
-
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services
-    .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights()
-    .AddTelemetryRegistration((IConfigurationRoot)builder.Configuration)
-    .AddApplicationRegistrations(builder.Configuration);
-
-builder.Services.Configure<LoggerFilterOptions>(options =>
+[ExcludeFromCodeCoverage]
+public static class Program
 {
-    var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
-        == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
-
-    if (defaultRule is not null)
+    public static async Task Main(string[] args)
     {
-        options.Rules.Remove(defaultRule);
+        FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
+
+        builder.AddConfiguration();
+
+        builder.ConfigureFunctionsWebApplication();
+
+        builder.Services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights()
+            .AddTelemetryRegistration((IConfigurationRoot)builder.Configuration)
+            .AddApplicationRegistrations(builder.Configuration);
+
+        builder.Services.Configure<LoggerFilterOptions>(options =>
+        {
+            var defaultRule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+            if (defaultRule is not null)
+            {
+                options.Rules.Remove(defaultRule);
+            }
+        });
+
+        var app = builder.Build();
+
+        await app.RunAsync();
     }
-});
-
-var app = builder.Build();
-
-await app.RunAsync();
+}
